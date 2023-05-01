a37ef600bf1b944bfb1f7277eb5aa7820efd98b8
tsukkiame <diazlins@gmail.com>
2023-05-01 17:15:01 -0300

    Adição de log

--- DebugLogginExp/src/Calculadora.java
+++ DebugLogginExp/src/Calculadora.java
@@ -1,18 +1,35 @@
+import java.io.IOException;
+import java.util.logging.Level;
 
 public class Calculadora {
-	public float calcular(float a, float b, char operacao) {
+	public float calcular(float a, float b, char operacao) throws IOException {
+		Log logCalc = new Log("Log.txt");		
+		try {
+			logCalc.logger.setLevel(Level.INFO);
+			logCalc.logger.info("Log de informação");
+			logCalc.logger.warning("Log de Aviso");
+			logCalc.logger.severe("Log Severo");
+		} catch (Exception e) {
+			logCalc.logger.warning("Aviso de execução da calculadora: "+e.getMessage());
+			System.out.println("Falha: " + e);
+		}
+		
 		float resultado = 0;
+		//adicionado break; faltando nos cases
 		switch (operacao) {
 		case '+': {
 			resultado = a + b;
 			break;
 		}
 		case '-': {
+			//verificando qual dos números tem valor maior (e aplicando "módulo") e inserindo qual "caso" é no log
 			if (a>b){
 				resultado = a - b;
+				logCalc.logger.info("O 1º valor digitado é maior.");
 			}
 			if (b>a){
 				resultado = b - a;
+				logCalc.logger.info("O 2º valor digitado é maior.");
 			}
 			break;
 		}
@@ -27,6 +44,6 @@ public class Calculadora {
 
 		}
 		return resultado;
-
+		
 	}
 }
--- DebugLogginExp/src/Principal.java
+++ DebugLogginExp/src/Principal.java
@@ -1,8 +1,20 @@
 import java.util.Scanner;
+import java.util.logging.*;
+import java.io.*;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Log log = new Log("Log.txt");
+		try {
+			log.logger.setLevel(Level.INFO);
+			log.logger.info("Log de informação");
+			log.logger.warning("Log de Aviso");
+			log.logger.severe("Log Severo");
+		} catch (Exception e) {
+			log.logger.warning("Aviso de exceção: "+e.getMessage());
+			System.out.println("Falha: " + e);
+		}
 		
 		try (Scanner leitura = new Scanner(System.in)) {
 			Calculadora c = new Calculadora();
@@ -19,9 +31,35 @@ public class Principal {
 			a = leitura.nextFloat();
 			System.out.println("Insira o 2o valor");
 			b = leitura.nextFloat();
-
 			resultado = c.calcular(a, b, operador.charAt(0));
 			System.out.println("O resultado de " + a + " " + operador + " " + b + " �:" + resultado);
+
+
+			//Adicioando o tipo de operação realizada dentro do obj operador (não funcionou)
+			/*switch (operador) {
+			case '+': {
+				operador. = 'Adição';
+				break;
+			}
+			
+			case '-': {
+				operador = 'Subtração';
+				break;
+			}
+			
+			case '*': {
+				operador = 'Multiplicação';
+				break;
+			}
+			
+			case '/': {
+				operador = 'Divisão';
+				break;
+			}
+			
+			}*/
+			//Log dos dados inseridos e o tipo de operação
+			log.logger.info("Operação realizada: "+operador+ "\n1º Valor inserido: "+a+"\n2º Valor inserido: "+b);
 		}
 
 	}
